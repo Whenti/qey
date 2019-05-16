76a57dea5b7d31d4ece4ac3d4687e2f78038d16e
Quentin Lévêque <quentin.leveque@bluebotics.com>
2019-05-16 19:17:07 +0200

    removing keyboard from qey

--- setup.py
+++ setup.py
@@ -13,34 +13,28 @@ HOME = os.path.expanduser("~")
 CONFIG_PATH = os.path.join(HOME,'.config')
 QEY_PATH = os.path.join(CONFIG_PATH,'qey')
 
-if not os.path.isdir(CONFIG_PATH):
-    os.mkdir(CONFIG_PATH)
 if not os.path.isdir(QEY_PATH):
+    if not os.path.isdir(CONFIG_PATH):
+        os.mkdir(CONFIG_PATH)
     os.mkdir(QEY_PATH)
 
-def addLines(filename, content, add):
-    with open(filename, "r+") as file:
-        for line in file:
-            if content in line:
-                return
-        file.write(add)
-
 if sys.platform in ['linux', 'linux2']:
-    #setup wh keyboard
-    KEYBOARD = os.path.join(current_path,'linux','keyboard')
-    XKB = "/usr/share/X11/xkb"
-    copyfile(os.path.join(KEYBOARD,'wh'), os.path.join(XKB,'symbols','wh'))
-    copyfile(os.path.join(KEYBOARD,'evdev.xml'), os.path.join(XKB,'rules','evdev.xml'))
+    HOTKEYS = os.path.join(current_path, 'linux','hotkeys')
+    AUTOKEY_DATA = os.path.join(CONFIG_PATH,'autokey','data')
 
     #setup space autokey
-    HOTKEYS = os.path.join(current_path, 'linux','hotkeys')
     with open(os.path.join(HOTKEYS,'space.py'), 'w', encoding='utf-8') as f:
         terminal_pop = os.path.join(current_path,"linux","terminal_pop.sh")
         to_write = "import os\nos.system('sh {}')".format(terminal_pop)
         f.write(to_write)
 
+    #init
+    if not os.path.isdir(AUTOKEY_DATA):
+        os.mkdir(AUTOKEY_DATA)
+    else:
+        os.system("rm -r {autokey_data}/*")
+        
     #setup autokey
-    AUTOKEY_DATA = os.path.join(os.environ['HOME'],'.config','autokey','data')
     os.system("cp -r {hotkeys} {autokey_data}".format(hotkeys = HOTKEYS, autokey_data=AUTOKEY_DATA))
 
     #make autokey start automatically
@@ -50,6 +44,13 @@ if sys.platform in ['linux', 'linux2']:
 python3 {}
 #QEY_END
 """.format(os.path.join(current_path,"python","start.py"))
+
+    def addLines(filename, content, add):
+    with open(filename, "r+") as file:
+        for line in file:
+            if content in line:
+                return
+        file.write(add)
     addLines("{}/.profile".format(os.environ['HOME']), content_flag, add_profile)
 
 else:
